6817d1d7c9787a416195b46acffbbaba86852cc6
samuelvictorol <samuel.softdev@outlook.com>
2022-04-14 00:13:20 -0300

    Solucao do exercicio OO

---
+++ Banco.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace ProgramOO
+{
+    internal class Conta
+    {
+        private int numeroConta;
+        private string titular;
+        private float saldo;
+        private List<string> extrato = new List<string>();
+        public Conta(int numeroConta)
+        {
+            this.numeroConta = numeroConta;
+        }
+
+        public string Titular
+        {
+            get
+            { 
+                return this.titular; 
+            }
+            set { this.titular = value; }
+        }
+
+        public void Depositar(float deposito)
+        {
+            this.extrato.Add($"voce depositou: {deposito}");
+            this.saldo += deposito;
+        }
+
+        public void Sacar(float saque)
+        {
+            this.extrato.Add($"voce sacou: {saque}");
+            this.saldo -= saque;  
+        }
+        
+        public void Extrato()
+        {
+            Console.WriteLine("------------EXTRATO-------------");
+            foreach (string item in this.extrato)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("--------------------------------");
+
+        }
+        public void Info()
+        {
+            Console.WriteLine("----------------------------------------------------------------------------------");
+            Console.WriteLine($"Numero da conta: {this.numeroConta}\tTitular: {this.Titular}\tSaldo: {this.saldo}");
+            Console.WriteLine("----------------------------------------------------------------------------------");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace ProgramOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool app = true;
+            Console.WriteLine("REGISTRE SUA NOVA CONTA\nOBS: utilize virgula para centavos\n");
+
+            Console.WriteLine("-> Digite o numero da conta:");
+            Conta conta = new Conta(int.Parse(Console.ReadLine()));
+
+            Console.WriteLine("-> Digite o nome do Titular:");
+            conta.Titular = Console.ReadLine();
+
+            Console.WriteLine("-> Deseja fazer um deposito inicial ?(s/n)");
+            switch (Console.ReadLine())
+            {
+                case "s":
+                    Console.WriteLine("-> Digite o valor a ser depositado: ");
+                    conta.Depositar(float.Parse(Console.ReadLine()));
+                    break;
+
+                case "n":
+                    break;
+
+                default:
+                    Console.WriteLine("Inválido");
+                    return;
+                    break;
+            }
+            Console.Clear();
+            conta.Info();
+            
+            Console.WriteLine("-> Digite o Valor do deposito: ");
+            conta.Depositar(float.Parse(Console.ReadLine()));
+            conta.Info();
+
+            Console.WriteLine("-> Digite o valor do saque: ");
+            conta.Sacar(float.Parse(Console.ReadLine()));
+            conta.Info();
+
+            Console.Clear();
+            Loading();
+            conta.Extrato();
+
+            Console.ReadKey();
+        }
+        static void Loading()
+        {
+            Console.WriteLine("Carregando seu Extrato");
+            for (int i = 0; i < 10; i++)
+            { 
+                Console.Write(".");
+                Thread.Sleep(500);
+            }
+            Console.Clear();
+        }
+    }
+}
